60d7796883aaabff5c28b3b395b1f5ec4c8f6249
Orbit. <159073116+1056516@users.noreply.github.com>
2025-02-02 16:22:08 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,5 @@
+// config.php
+return [
+    'MAILGUN_API_KEY' => '6ead62b5a9af685f57b87343da154d81-d8df908e-da93db9a',
+    'MAILGUN_DOMAIN' => 'sandbox200e5e5e6ffe412b95ec54b9d213e1f0.mailgun.org'
+];
